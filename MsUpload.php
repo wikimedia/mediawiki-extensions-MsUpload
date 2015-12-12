b612d925b4083115e21964b5c05230b944a08f01
Paladox <thomasmulhall410@yahoo.com>
2015-12-12 21:42:20 +0000

    Remove php entry point
    Change-Id: I052b1e8881802a27548ec517e5adb7dd6bd92c5c
    (cherry picked from commit 7fb7b2c77ac214295515b5d2b7b917696f5be201)

--- MsUpload.php
+++ MsUpload.php
@@ -1,54 +1,15 @@
 <?php
 
-$wgExtensionCredits['parserhook'][] = array(
-	'name' => 'MsUpload',
-	'url' => 'http://www.mediawiki.org/wiki/Extension:MsUpload',
-	'version' => '12.0',
-	'descriptionmsg' => 'msu-desc',
-	'license-name' => 'GPL-2.0+',
-	'author' => array( '[mailto:wiki@ratin.de Martin Schwindl]', '[mailto:wiki@keyler-consult.de Martin Keyler]', '[https://www.mediawiki.org/wiki/User:Luis_Felipe_Schenone Luis Felipe Schenone]' ),
-);
-
-$wgResourceModules['ext.MsUpload'] = array(
-	'scripts' => array(
-		'plupload/plupload.full.min.js',
-		'MsUpload.js'
-	),
-	'dependencies' => 'jquery.ui.progressbar',
-	'styles' => 'MsUpload.css',
-	'messages' => array(
-		'msu-button-title',
-		'msu-insert-link',
-		'msu-insert-gallery',
-		'msu-insert-files',
-		'msu-insert-links',
-		'msu-insert-image',
-		'msu-insert-video',
-		'msu-cancel-upload',
-		'msu-replace-file',
-		'msu-clean-all',
-		'msu-ext-not-allowed',
-		'msu-upload-this',
-		'msu-upload-all',
-		'msu-dropzone',
-		'msu-comment',
-	),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MsUpload',
-);
-
-$wgMessagesDirs['MsUpload'] = __DIR__ . '/i18n';
-
-$wgAutoloadClasses['MsUpload'] = __DIR__ . '/MsUpload.body.php';
-
-$wgHooks['EditPage::showEditForm:initial'][] = 'MsUpload::start';
-
-$wgAjaxExportList[] = 'MsUpload::saveCat';
-
-// Default configuration
-$wgMSU_useDragDrop = true;
-$wgMSU_showAutoCat = true;
-$wgMSU_checkAutoCat = true;
-$wgMSU_useMsLinks = false;
-$wgMSU_confirmReplace = true;
-$wgMSU_imgParams = '400px';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MsUpload' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['MsUpload'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for MsUpload extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the MsUpload extension requires MediaWiki 1.25+' );
+}
