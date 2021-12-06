b9feee219d327855340328bac4994139e852dd07
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 16:32:55 +0100

    Emit deprecation warning from php entry point
    Change-Id: I0cdec702dd5ade264ab70e8ffe74fcd5f9c6159a

--- MsUpload.php
+++ MsUpload.php
@@ -4,12 +4,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'MsUpload' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['MsUpload'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for MsUpload extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the MsUpload extension requires MediaWiki 1.25+' );
+	die( 'This version of the MsUpload extension requires MediaWiki 1.32+' );
 }
